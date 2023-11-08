2184b3b7ebfb8ae9b1400608318beb6e014f39b9
dinooo <145918377+dindooo@users.noreply.github.com>
2023-11-08 09:07:30 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrviZadatak1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a=2, b=4, c=6, d=8;
+            double x = (a*a+b*b+c*c+d*d)/(a+b)*(a+b)+(c+d)*(c+d);
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+
+    }
+    
+}
